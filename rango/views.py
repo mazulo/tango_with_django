165ef5fa6b1834cdb52b53827b895af58f42d43a
Patrick Mazulo <pmazulo@gmail.com>
2015-04-01 16:05:06 -0300

    "Updated to insert templates files."

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,21 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+
+    # Construir um dicionário para passar para o motor do template
+    # como seu contexto.
+    # Note que a chave boldmessage é a mesma {{ boldmessage }} no template!
+    context_dict = {'boldmessage': 'Eu sou a fonte em negrito do contexto.'}
+
+    # Retorna uma resposta (response) renderizada para enviar ao cliente.
+    # Nós usamos a função atalho para tornar nosso trabalho mais fácil.
+    # Note que o primeiro parâmetro é o template que desejamos usar.
+    return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    context_dict = {'message': 'Not necessary'}
+    return render(request, 'rango/about.html', context_dict)
